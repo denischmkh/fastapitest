19b7414b5fba86842403e05d975e7ae06df04cdd
denischmkh <avraam.l2004@gmail.com>
2024-08-07 18:13:45 +0300

    NEW Func

--- main.py
+++ main.py
@@ -19,3 +19,8 @@ class Item(BaseModel):
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
