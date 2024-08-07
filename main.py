92bda7ca2ece131d3ce4bb7326aaa9ae62586a12
denischmkh <avraam.l2004@gmail.com>
2024-08-07 18:25:25 +0300

    NEW Func

--- main.py
+++ main.py
@@ -22,5 +22,4 @@ async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+    return None
