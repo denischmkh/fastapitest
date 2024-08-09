958e3f045197086b42691563bd8a263db16982d1
denischmkh <avraam.l2004@gmail.com>
2024-08-09 13:03:03 +0300

    NEW Project

--- database/crud.py
+++
@@ -1,4 +0,0 @@
-def get_all_dbase_items():
-    return [{'id': 1, 'title': 'item1'},
-            {'id': 2, 'title': 'item2'},
-            {'id': 3, 'title': 'item3'}]
--- functions/functions.py
+++
@@ -1,5 +0,0 @@
-from ..database.crud import get_all_dbase_items
-
-def get_items():
-    result = get_all_dbase_items()
-    return result
